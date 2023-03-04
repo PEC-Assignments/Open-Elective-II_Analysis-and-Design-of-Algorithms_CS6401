61c36b961b15169831664bb803f11526f7e93d4b
Manobal Singh Bagady <manobalsinghbagady.bt21ele@pec.edu.in>
2023-03-04 06:32:24 +0530

    Create Answer 2.py

---
+++ Lab Assignment 2/Answer 2.py
@@ -0,0 +1,23 @@
+import sys
+sys.stdin = open("input.txt", "r")
+sys.stdout = open("output.txt", "w")
+sys.stderr = open("error.txt", "w")
+# Comment the above 4 lines if you are not using file input output.
+
+
+def pow(i, e):
+    if e == 0:
+        return 1
+    elif e == 1:
+        return i
+    else:
+        return i * pow(i, e - 1)
+
+
+if __name__ == "__main__":
+    num, exp = map(int, input().strip().split())
+    print(pow(num, exp))
+
+# Complexity Analysis of the above solution:
+# Time Complexity: O(n)
+# Space Complexity: O(1)
