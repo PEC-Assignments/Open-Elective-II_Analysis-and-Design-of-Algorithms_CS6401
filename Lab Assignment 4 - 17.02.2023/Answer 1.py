cad30e38d1140d89390d7aabc47863f01e96d4a4
Manobal Singh Bagady <manobalsinghbagady.bt21ele@pec.edu.in>
2023-03-31 13:42:20 +0530

    Create Answer 1.py

---
+++ Lab Assignment 4 - 17.02.2023/Answer 1.py
@@ -0,0 +1,38 @@
+# ---------------- MSB's Coding Template ---------------- #
+'''
+    "ɴᴏ ᴄᴏᴅᴇ ɪꜱ ᴘᴇʀꜰᴇᴄᴛ"
+'''
+
+# ---I/O from file---#
+import sys
+try:
+    sys.stdin = open("input.txt", "r")
+    sys.stdout = open("output.txt", "w")
+    sys.stderr = open("output.txt", "w")
+except:
+    pass
+
+
+# ---------------------- Code Starts Here ----------------------#
+
+def sortedInsert(s, element):
+    if len(s) == 0 or element > s[-1]:
+        s.append(element)
+        return
+    else:
+        temp = s.pop()
+        sortedInsert(s, element)
+        s.append(temp)
+
+
+def sortStack(s):
+    if len(s) != 0:
+        temp = s.pop()
+        sortStack(s)
+        sortedInsert(s, temp)
+
+
+if __name__ == "__main__":
+    s = [30, -5, 18, 14, -3]
+    sortStack(s)
+    print(s)
