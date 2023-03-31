9912e2355d4b224a05bb815de00aa9653f01e78c
Manobal Singh Bagady <manobalsinghbagady.bt21ele@pec.edu.in>
2023-03-31 16:10:31 +0530

    Create Answer 1 - check palindrome.py

---
+++ Lab Assignment 2 - 27.01.2023/Answer 1 - check palindrome.py
@@ -0,0 +1,49 @@
+# Solution to Question 1 - Check palindrome.
+
+# ---------------- MSB's Coding Template ---------------- #
+'''
+    "ɴᴏ ᴄᴏᴅᴇ ɪꜱ ᴘᴇʀꜰᴇᴄᴛ"
+'''
+
+# ---I/O from file---#
+import sys
+try:
+    sys.stdin = open("input.txt", "r")
+    sys.stdout = open("output.txt", "w")
+    sys.stderr = open("output.txt", "w")
+except:
+    pass
+
+
+# ---------------------- Code Starts Here ----------------------#
+
+
+def check_palindrome(s):
+
+    # General solution
+    if len(s) == 0 or len(s) == 1:
+        return "True"
+    else:
+        for i in range(len(s)//2):
+            if s[i] != s[-i-1]:
+                return "False"
+        return "True"
+
+    # Python specific solution
+    '''
+    if len(s) == 0 or len(s) == 1:
+        return "True"
+    else:
+        if s == s[::-1]:
+            return "True"
+        else:
+            return "False"
+    '''
+
+
+if __name__ == "__main__":
+    print(check_palindrome(input().strip().lower()))
+
+# Complexity Analysis of the above solution:
+# Time Complexity: O(n)
+# Space Complexity: O(1)
