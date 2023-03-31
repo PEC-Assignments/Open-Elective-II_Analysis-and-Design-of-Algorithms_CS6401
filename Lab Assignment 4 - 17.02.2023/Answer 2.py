4bedfd31d2e341fbcdf00c53c69194d6eddaf825
Manobal Singh Bagady <manobalsinghbagady.bt21ele@pec.edu.in>
2023-03-31 13:42:23 +0530

    Create Answer 2.py

---
+++ Lab Assignment 4 - 17.02.2023/Answer 2.py
@@ -0,0 +1,35 @@
+# ---------------- MSB's Coding Template ---------------- #
+'''
+    "ɴᴏ ᴄᴏᴅᴇ ɪꜱ ᴘᴇʀꜰᴇᴄᴛ"
+'''
+
+# ---I/O from file---#
+import sys
+try:
+    sys.stdin = open("input.txt", "r")
+    sys.stdout = open("output.txt", "w")
+    sys.stderr = open("output.txt", "w")
+except:
+    pass
+
+
+# ---------------------- Code Starts Here ----------------------#
+
+def isMatch(text: str, pattern: str) -> bool:
+    if not pattern:
+        return not text
+
+    first_match = bool(text) and pattern[0] in {text[0], '.'}
+
+    if len(pattern) >= 2 and pattern[1] == '*':
+        return (isMatch(text, pattern[2:]) or
+                first_match and isMatch(text[1:], pattern))
+    else:
+        return first_match and isMatch(text[1:], pattern[1:])
+
+
+if __name__ == "__main__":
+    text = "abbbc"
+    pattern = "ab*c"
+    result = isMatch(text, pattern)
+    print(result)
