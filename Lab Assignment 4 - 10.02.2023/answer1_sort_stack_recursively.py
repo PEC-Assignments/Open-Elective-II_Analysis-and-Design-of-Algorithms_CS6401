f4e896d049665ab49db5ef9095901df5d535f455
Manobal Singh Bagady <manobalsinghbagady.bt21ele@pec.edu.in>
2023-04-23 02:21:00 +0530

    Assignment 4 - Sort Stack Recursively

---
+++ Lab Assignment 4 - 10.02.2023/answer1_sort_stack_recursively.py
@@ -0,0 +1,55 @@
+# Solution Question 1 - Sort Stack
+
+# ---------------- MSB's Coding Template ---------------- #
+"""
+    "ɴᴏ ᴄᴏᴅᴇ ɪꜱ ᴘᴇʀꜰᴇᴄᴛ"
+"""
+
+# ---I/O from file---#
+import sys
+
+try:
+    sys.stdin = open("input.txt", "r", encoding="UTF-8")
+    sys.stdout = open("output.txt", "w", encoding="UTF-8")
+    sys.stderr = open("output.txt", "w", encoding="UTF-8")
+except FileNotFoundError as e:
+    pass
+
+# ---------------------- Code Starts Here ----------------------#
+
+
+def sorted_insert(stack: list[int], element: int) -> None:
+    """a helper function to insert an element in a sorted manner in a stack.
+
+    Args:
+        stack (list[int]): the stack in which the element is to be inserted
+        element (int): the element to be inserted
+    """
+    if len(stack) == 0 or element > stack[-1]:
+        stack.append(element)
+        return
+    temp = stack.pop()
+    sorted_insert(stack, element)
+    stack.append(temp)
+
+
+def sort_stack(stack: list[int]) -> None:
+    """a function to sort a stack using recursion.
+
+    Args:
+        stack (list[int]): the stack to be sorted
+    """
+    if len(stack) != 0:
+        temp = stack.pop()
+        sort_stack(stack)
+        sorted_insert(stack, temp)
+
+
+if __name__ == "__main__":
+    s = [30, -5, 18, 14, -3]
+    sort_stack(s)
+    print(s)
+
+# Complexity Analysis of the above solution:
+# Time Complexity: O(n^2)
+# Space Complexity: O(n)
